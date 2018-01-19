69ebe61959ebad809f5c293299d925ea2efeabd3
Cody Moss <codytyler.moss@gmail.com>
2018-01-18 22:17:55 -0700

    initial commit

--- QR Code Generator/MainWindow.xaml.cs
+++ QR Code Generator/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QR_Code_Generator
 {
@@ -20,9 +12,43 @@ namespace QR_Code_Generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        private void _BTN_Encode_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(_TB_Encode_Input.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            _IMG_Preview.Source = Convert(qrCodeImage); 
+        }
+
+        private void _BTN_Save_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Takes a bitmap and converts it to an image that can be handled by WPF ImageBrush
+        /// </summary>
+        /// <param name="src">A bitmap image</param>
+        /// <returns>The image as a BitmapImage for WPF</returns>
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
     }
 }
---
+++ QR Code Generator/README.md
@@ -0,0 +1,15 @@
+﻿# QR Code Generator
+
+This program allows users to create custom QR codes. 
+
+I plan on adding some additional functionality to spice things up. 
+
+
+## Todo:
+
+- [ ] Implement save function 
+- [ ] Save QR Codes in multiple formats
+- [ ] choose save location
+- [ ] Give options to choose qr code colors
+- [ ] Give the option to embed images into QR code  
+
